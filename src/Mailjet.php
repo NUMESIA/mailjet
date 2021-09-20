09eefca598ba986774859ee496306c4334f06bd8
zorx <oudjehih@zedx.org>
2021-09-20 17:18:03 +0200

    feat: can send an email to multiple recipients

--- src/Mailjet.php
+++ src/Mailjet.php
@@ -13,12 +13,12 @@ class Mailjet
     /**
      * Send the email.
      *
-     * @param  string  $to
+     * @param  string|array  $to
      * @param  array  $data
      * @param  string|null  $sender
      * @return \Mailjet\Response|null
      */
-    public function sendEmail(string $to, array $data)
+    public function sendEmail($to, array $data)
     {
         return MailjetBase::post(Resources::$Email, $this->formatRequest($to, $data));
     }
@@ -26,19 +26,27 @@ class Mailjet
     /**
      * Get the form params.
      *
-     * @param  string  $to
+     * @param  string|array  $to
      * @param  array  $data
      * @param  string|null  $sender
      * @return array
      */
-    protected function formatRequest(string $to, array $data): array
+    protected function formatRequest($to, array $data): array
     {
+        $to = is_array($to) ? $to : [$to];
+
+        $recipients = array_map(function($recipient) {
+            return [
+                'Email' => $recipient
+            ];
+        }, $to);
+
         $body = [
             'FromEmail'  => data_get($data, "sender") ?: env('MAIL_FROM_ADDRESS'),
             'FromName'   => data_get($data, "name") ?: env('MAIL_FROM_NAME'),
             'Subject'    => data_get($data, "subject"),
             'Html-part'  => data_get($data, "content"),
-            'Recipients' => [['Email' => $to]],
+            'Recipients' => $recipients,
         ];
 
         return [
